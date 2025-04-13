5093675a4b6ec9b0fa50fd5cd49b0762857ea75a
JARHOU <jarhou06@gmail.com>
2025-04-13 21:31:34 +0200

    ajout foctionalité denvoie donné user et verification si mot de passe et le meme

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -6,7 +6,7 @@ import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import UpdateProfile from "./components/UpdateProfile";
 import ForgotPassword from "./components/ForgotPassword";
-
+import AuthProvider from "./context/AuthContext";
 
 function App() {
   return (
@@ -14,22 +14,19 @@ function App() {
       className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}
     >
-      <div className="w-100" style={{maxWidth:"400px"}}>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
         <Router>
+          <AuthProvider>
           <Routes>
-            <Route path="/signup" element={<Signup/>}/>
-            <Route path="/login" element={<Login/>}/>
-          <Route path="/forgot-password" element={<ForgotPassword/>}/>
-            <Route path="/update-profile" element={<UpdateProfile/>}/>
-            <Route path="/" element={<Dashboard/>}/>
-
-
-
-
-
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/update-profile" element={<UpdateProfile />} />
+            <Route path="/" element={<Dashboard />} />
           </Routes>
+          </AuthProvider>
+         
         </Router>
-
       </div>
     </Container>
   );
--- firebase/src/components/Signup.js
+++ firebase/src/components/Signup.js
@@ -1,44 +1,75 @@
-import React from 'react'
-import { Button, Card, Form } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import {React, useRef, useState } from "react";
+import { Alert, Button, Card, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const Signup = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passworConfirmationdRef = useRef();
+  const { signup } = useAuth();
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (passwordRef.current.value !== passworConfirmationdRef.current.value) {
+      return setError("Passwords do not match");
+    }
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Failed to create an account");
+    }
+    setLoading(false);
+  }
   return (
     <>
-    <Card>
+      <Card>
         <Card.Body>
-            <h2 className='text-center mb-4'>
-              signup  
-            </h2>
-            <Form>
-                <Form.Group>
-                    <Form.Label htmlFor='email'>Email</Form.Label>
-                    <Form.Control type="email" id="email"/>
-                </Form.Group>
+          <h2 className="text-center mb-4">signup</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Control type="email" id="email" ref={emailRef} />
+            </Form.Group>
 
-                <Form.Group>
-                    <Form.Label htmlFor='password'>Password</Form.Label>
-                    <Form.Control type="password" id="password"/>
-                </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="password">Password</Form.Label>
+              <Form.Control type="password" id="password" ref={passwordRef} />
+            </Form.Group>
 
-                <Form.Group>
-                    <Form.Label htmlFor='password-confirmation'>Password confirmation</Form.Label>
-                    <Form.Control type="password" id="password-confirmation"/>
-                </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="password-confirmation">
+                Password confirmation
+              </Form.Label>
+              <Form.Control
+                type="password"
+                id="password-confirmation"
+                ref={passworConfirmationdRef}
+              />
+            </Form.Group>
 
-                <Button variant="primary" type="submit"className='w-100 mt-3'> 
-                   Signup
-                </Button>
-            </Form>
+            <Button
+              variant="primary"
+              type="submit"
+              className="w-100 mt-3"
+              disabled={loading}
+            >
+              Signup
+            </Button>
+          </Form>
         </Card.Body>
-
-    </Card>
-    <div className='w-100 text-center mt-2'> 
+      </Card>
+      <div className="w-100 text-center mt-2">
         already have an account ? <Link to="/login">Login</Link>
-
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
